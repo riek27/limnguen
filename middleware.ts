02656132ee2fd62b9f39738beb4afd6e318b18e9
Riek Gatluak <riekgatluak25@gmail.com>
2026-09-23 05:04:44 -0700

    Exclude sitemap.xml and robots.txt from middleware

--- middleware.ts
+++ middleware.ts
@@ -18,5 +18,18 @@ export function middleware(request: NextRequest) {
 }
 
 export const config = {
-  matcher: '/((?!api|_next/static|_next/image|favicon.ico).*)',
+  matcher: [
+    /*
+     * Match all request paths except for the ones starting with:
+     * - api (API routes)
+     * - _next/static (static files)
+     * - _next/image (image optimization files)
+     * - favicon.ico (favicon file)
+     * - sitemap.xml (sitemap)
+     * - robots.txt (robots file)
+     * - images (public images)
+     * - uploads (public uploads)
+     */
+    '/((?!api|_next/static|_next/image|favicon.ico|sitemap.xml|robots.txt|images|uploads).*)',
+  ],
 };
